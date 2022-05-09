dc3a66be8ac90e18967fcf159581294cc1e62957
juni <woajun@naver.com>
2022-05-09 10:52:31 +0900

    샘플 중복코드 반복문 처리

--- src/pages/Sample.vue
+++ src/pages/Sample.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
-    <button @click="view = 'SearchSettingAll'">샘플</button>
-    <button @click="view = '검수입고관리'">검수입고관리</button>
-    <button @click="view = '정수물품책정현황'">정수물품책정현황</button>
-    <div v-show="view === 'SearchSettingAll'">
-      <sl :search-setting="SearchSettingAll" :table-setting="tableSetting" />
-    </div>
-    <div v-show="view === '검수입고관리'">
-      <sl :search-setting="검수입고관리" :table-setting="tableSetting" />
-    </div>
-    <div v-show="view === '정수물품책정현황'">
-      <sl :search-setting="정수물품책정현황" :table-setting="tableSetting" />
-    </div>
+    <template v-for="(view, key) of views" :key="key">
+      <button @click="currentView = key">{{ key }}</button>
+    </template>
+    <template v-for="(view, key) of views" :key="key">
+      <div v-show="currentView === key">
+        <sl :search-setting="view" :table-setting="tableSetting" />
+      </div>
+    </template>
   </div>
 </template>
 
@@ -19,9 +15,17 @@
 import SearchSettingAll from "./settings/SearchSettingAll";
 import 검수입고관리 from "./settings/검수입고관리";
 import 정수물품책정현황 from "./settings/정수물품책정현황";
+import 코드 from "./settings/Code";
 import { tableSetting } from "./settings/sampleTableSetting";
 import sl from "../layouts/SL.vue";
 import { ref } from "vue";
 
-const view = ref("SearchSettingAll");
+const currentView = ref("SearchSettingAll");
+
+const views = {
+  SearchSettingAll: SearchSettingAll,
+  검수입고관리: 검수입고관리,
+  정수물품책정현황: 정수물품책정현황,
+  코드: 코드,
+};
 </script>
