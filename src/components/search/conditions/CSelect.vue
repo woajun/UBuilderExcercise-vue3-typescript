87beb5c1d48ce8bdf1e15375287f355489d2a389
juni <woajun@naver.com>
2022-05-12 13:41:28 +0900

    select 깊이재기

--- src/components/search/conditions/CSelect.vue
+++ src/components/search/conditions/CSelect.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <template v-if="label"> {{ label }} : </template>
+    <button @click.prevent="calcDepth(options)">깊이재기</button>
     <CSelectSelect
       :placeholder="placeholder"
       :initial-value="initialValue"
@@ -21,10 +22,10 @@ defineProps<{
   kind: "select";
   width?: string;
   field: string;
-  searchItem: string[];
+  searchItem?: string[];
   label?: string;
   initialValue?: string | string[];
-  options: Array<Option>;
+  options: Option[];
   placeholder?: string | string[];
 }>();
 
@@ -34,4 +35,26 @@ const emit = defineEmits(["update:searchItem"]);
 watch(items, (newItems) => {
   emit("update:searchItem", newItems);
 });
+
+function calcDepth(options: Option[]): number {
+  const depthest = Math.max(...arrDepth(options), 0);
+  console.log(depthest);
+  return depthest;
+
+  function arrDepth(options: Option[]): number[] {
+    const result: number[] = [];
+    aDepth(0, options, result);
+    return result;
+  }
+  function aDepth(d: number, array: Option[], result: number[]) {
+    d++;
+    result.push(d);
+    for (let i = 0; i < array.length; i++) {
+      const e = array[i];
+      if (e.children) {
+        aDepth(d, e.children, result);
+      }
+    }
+  }
+}
 </script>
