9644808a0df890ddcb9c65964c0ec7a3b3958c0d
juni <woajun@naver.com>
2022-05-13 15:32:11 +0900

    Select.ts 변경

--- src/pages/settings/Select.ts
+++ src/pages/settings/Select.ts
@@ -5,254 +5,142 @@ const searchSetting: SearchSetting = {
   conditions: [
     {
       kind: "select",
-      label: "물품분류",
-      field: "slt4",
-      placeholder: "분류",
-      options: [
-        {
-          description: "A",
-          value: "A",
+      label: "학년선택",
+      placeholder: "이름",
+      field: "fld_grade",
+      description: "name",
+      value: "grade",
+      nestedSelect: {
+        data: "classes",
+        placeholder: "반",
+        field: "fld_class",
+        description: "name",
+        value: "class",
+        nestedSelect: {
+          data: "students",
+          placeholder: "이름",
+          field: "fld_student",
+          description: "name",
+          value: "id",
         },
+      },
+      data: [
         {
-          description: "B",
-          value: "B",
-        },
-        {
-          description: "C",
-          value: "C",
-        },
-      ],
-    },
-    {
-      kind: "select",
-      label: "물품분류",
-      field: "slt1",
-      initialValue: "A",
-      placeholder: "분류",
-      options: [
-        {
-          description: "A",
-          value: "A",
-        },
-        {
-          description: "B",
-          value: "B",
-        },
-        {
-          description: "C",
-          value: "C",
-        },
-      ],
-    },
-    {
-      kind: "select",
-      label: "물품분류",
-      initialValue: ["A", "Aa", "Aa가"],
-      placeholder: ["1차분류", "2차분류", "3차분류"],
-      field: "slt2",
-      options: [
-        {
-          description: "A",
-          value: "A",
-          children: [
+          name: "1학년",
+          grade: 1,
+          classes: [
             {
-              description: "Aa",
-              value: "Aa",
-              children: [
-                { description: "Aa가", value: "Aa가" },
-                { description: "Aa나", value: "Aa나" },
-                { description: "Aa다", value: "Aa다" },
+              name: "무지개반",
+              class: 1,
+              students: [
+                {
+                  name: "홍길동",
+                  id: 10101,
+                },
+                {
+                  name: "이순신",
+                  id: 10102,
+                },
+                {
+                  label: "유재석",
+                  id: 10103,
+                },
               ],
             },
             {
-              description: "Ab",
-              value: "Ab",
-              children: [
-                { description: "Ab가", value: "Ab가" },
-                { description: "Ab나", value: "Ab나" },
-                { description: "Ab다", value: "Ab다" },
+              name: "늘푸른반",
+              class: 2,
+              students: [
+                {
+                  name: "가나가",
+                  id: 10201,
+                },
+                {
+                  name: "가나나",
+                  id: 10202,
+                },
+                {
+                  label: "가나다",
+                  id: 10203,
+                },
               ],
             },
             {
-              description: "Ac",
-              value: "Ac",
-              children: [
-                { description: "Ac가", value: "Ac가" },
-                { description: "Ac나", value: "Ac나" },
-                { description: "Ac다", value: "Ac다" },
+              name: "해돋이반",
+              class: 3,
+              students: [
+                {
+                  name: "가다가",
+                  id: 10301,
+                },
+                {
+                  name: "가다나",
+                  id: 10302,
+                },
+                {
+                  label: "가다다",
+                  id: 10303,
+                },
               ],
             },
           ],
         },
         {
-          description: "B",
-          value: "B",
-          children: [
+          name: "2학년",
+          grade: 2,
+          classes: [
             {
-              description: "Ba",
-              value: "Ba",
-              children: [
-                { description: "Ba가", value: "Ba가" },
-                { description: "Ba나", value: "Ba나" },
-                { description: "Ba다", value: "Ba다" },
+              name: "무지개반",
+              class: 1,
+              students: [
+                {
+                  name: "나가가",
+                  id: 20101,
+                },
+                {
+                  name: "나가나",
+                  id: 20102,
+                },
+                {
+                  label: "나가다",
+                  id: 20103,
+                },
               ],
             },
             {
-              description: "Bb",
-              value: "Bb",
-              children: [
-                { description: "Bb가", value: "Bb가" },
-                { description: "Bb나", value: "Bb나" },
-                { description: "Bb다", value: "Bb다" },
+              name: "늘푸른반",
+              class: 2,
+              students: [
+                {
+                  name: "나나가",
+                  id: 20201,
+                },
+                {
+                  name: "나나나",
+                  id: 20202,
+                },
+                {
+                  label: "나나다",
+                  id: 20203,
+                },
               ],
             },
             {
-              description: "Bc",
-              value: "Bc",
-              children: [
-                { description: "Bc가", value: "Bc가" },
-                { description: "Bc나", value: "Bc나" },
-                { description: "Bc다", value: "Bc다" },
-              ],
-            },
-          ],
-        },
-        {
-          description: "C",
-          value: "C",
-          children: [
-            {
-              description: "Ca",
-              value: "Ca",
-              children: [
-                { description: "Ca가", value: "Ca가" },
-                { description: "Ca나", value: "Ca나" },
-                { description: "Ca다", value: "Ca다" },
-              ],
-            },
-            {
-              description: "Cb",
-              value: "Cb",
-              children: [
-                { description: "Cb가", value: "Cb가" },
-                { description: "Cb나", value: "Cb나" },
-                { description: "Cb다", value: "Cb다" },
-              ],
-            },
-            {
-              description: "Cc",
-              value: "Cc",
-              children: [
-                { description: "Cc가", value: "Cc가" },
-                { description: "Cc나", value: "Cc나" },
-                { description: "Cc다", value: "Cc다" },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      kind: "select",
-      label: "물품분류",
-      placeholder: "물품분류",
-      field: "slt3",
-      options: [
-        {
-          description: "A",
-          value: "A",
-          children: [
-            {
-              description: "Aa",
-              value: "Aa",
-              children: [
-                { description: "Aa가", value: "Aa가" },
-                { description: "Aa나", value: "Aa나" },
-                { description: "Aa다", value: "Aa다" },
-              ],
-            },
-            {
-              description: "Ab",
-              value: "Ab",
-              children: [
-                { description: "Ab가", value: "Ab가" },
-                { description: "Ab나", value: "Ab나" },
-                { description: "Ab다", value: "Ab다" },
-              ],
-            },
-            {
-              description: "Ac",
-              value: "Ac",
-              children: [
-                { description: "Ac가", value: "Ac가" },
-                { description: "Ac나", value: "Ac나" },
-                { description: "Ac다", value: "Ac다" },
-              ],
-            },
-          ],
-        },
-        {
-          description: "B",
-          value: "B",
-          children: [
-            {
-              description: "Ba",
-              value: "Ba",
-              children: [
-                { description: "Ba가", value: "Ba가" },
-                { description: "Ba나", value: "Ba나" },
-                { description: "Ba다", value: "Ba다" },
-              ],
-            },
-            {
-              description: "Bb",
-              value: "Bb",
-              children: [
-                { description: "Bb가", value: "Bb가" },
-                { description: "Bb나", value: "Bb나" },
-                { description: "Bb다", value: "Bb다" },
-              ],
-            },
-            {
-              description: "Bc",
-              value: "Bc",
-              children: [
-                { description: "Bc가", value: "Bc가" },
-                { description: "Bc나", value: "Bc나" },
-                { description: "Bc다", value: "Bc다" },
-              ],
-            },
-          ],
-        },
-        {
-          description: "C",
-          value: "C",
-          children: [
-            {
-              description: "Ca",
-              value: "Ca",
-              children: [
-                { description: "Ca가", value: "Ca가" },
-                { description: "Ca나", value: "Ca나" },
-                { description: "Ca다", value: "Ca다" },
-              ],
-            },
-            {
-              description: "Cb",
-              value: "Cb",
-              children: [
-                { description: "Cb가", value: "Cb가" },
-                { description: "Cb나", value: "Cb나" },
-                { description: "Cb다", value: "Cb다" },
-              ],
-            },
-            {
-              description: "Cc",
-              value: "Cc",
-              children: [
-                { description: "Cc가", value: "Cc가" },
-                { description: "Cc나", value: "Cc나" },
-                { description: "Cc다", value: "Cc다" },
+              name: "해돋이반",
+              class: 3,
+              students: [
+                {
+                  name: "나다가",
+                  id: 20301,
+                },
+                {
+                  name: "나다나",
+                  id: 20302,
+                },
+                {
+                  label: "나다다",
+                  id: 20303,
+                },
               ],
             },
           ],
