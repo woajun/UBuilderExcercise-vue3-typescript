5b585e705fdf7cfad1655eb914ca8482eed7db06
juni <woajun@naver.com>
2022-05-23 09:28:00 +0900

    deleate multi select settings

--- src/pages/SampleMultiSelect.vue
+++ src/pages/SampleMultiSelect.vue
@@ -5,5 +5,55 @@
 </template>
 <script setup lang="ts">
 import CMultiSelect from "@/components/search/conditions/CMultiSelect.vue";
-import { selects } from "./SampleMultiSelectSetting";
+
+type Data = Array<Record<string, any>>;
+type Selected = Record<string, any>;
+
+const selects = [
+  {
+    valueKey: "value",
+    descriptionKey: "value",
+    field: "slt1",
+    data: data1,
+    label: "선택",
+  },
+  {
+    valueKey: "id",
+    descriptionKey: "name",
+    field: "slt2",
+    data: data2,
+  },
+  {
+    valueKey: "value",
+    descriptionKey: "value",
+    field: "slt3",
+    data: data3,
+  },
+];
+
+function data1(): Data {
+  return [
+    { description: "users", value: "users" },
+    { description: "comments", value: "comments" },
+  ];
+}
+
+function data2(selected: Selected): Promise<Data> {
+  const slt1 = selected["slt1"];
+  return searchFor(`https://jsonplaceholder.typicode.com/${slt1}`);
+
+  async function searchFor(url: string) {
+    const resp = await fetch(url);
+    return await resp.json();
+  }
+}
+
+function data3(selected: Selected): Data {
+  const slt1 = selected["slt1"];
+  const slt2 = selected["slt2"];
+
+  if (slt1 !== "users") return [];
+  if (!slt2) return [];
+  return [{ value: "male" }, { value: "female" }];
+}
 </script>
