7cfc9ee4fc658307f80fd2e00bcc2a6ec15bb741
juni <woajun@naver.com>
2022-05-23 15:20:41 +0900

    data 함수가 아닌  변수로 넘기기

--- src/pages/SampleMultiSelect.vue
+++ src/pages/SampleMultiSelect.vue
@@ -6,47 +6,56 @@
 </template>
 <script setup lang="ts">
 import CMultiSelect from "@/components/search/conditions/CMultiSelect.vue";
-import { ref } from "vue";
+import { reactive, ref, watch } from "vue";
 
 type Data = Array<Record<string, any>>;
 
 const searchItem = ref({
-  slt1: null,
-  slt2: null,
-  slt3: null,
+  slt1: "",
+  slt2: "",
+  slt3: "",
 });
 
-const selects = [
+const dataOne = [
+  { description: "users", value: "users" },
+  { description: "comments", value: "comments" },
+];
+const dataTwo = ref();
+const dataThree = ref();
+
+const selects = reactive([
   {
     valueKey: "value",
     descriptionKey: "value",
     field: "slt1",
-    data: data1,
+    data: dataOne,
     label: "선택",
   },
   {
     valueKey: "id",
     descriptionKey: "name",
     field: "slt2",
-    data: data2,
+    data: dataTwo,
   },
   {
     valueKey: "value",
     descriptionKey: "value",
     field: "slt3",
-    data: data3,
+    data: dataThree,
   },
-];
+]);
 
-function data1(): Data {
-  return [
-    { description: "users", value: "users" },
-    { description: "comments", value: "comments" },
-  ];
-}
+watch(
+  () => {
+    return { slt1: searchItem.value.slt1, slt2: searchItem.value.slt2 };
+  },
+  ({ slt1, slt2 }) => {
+    dataTwo.value = data2(slt1);
+    dataThree.value = data3(slt1, slt2);
+  }
+);
 
-function data2(): Promise<Data> {
-  const slt1 = searchItem.value.slt1;
+function data2(slt1: string): Promise<Data> {
   return searchFor(`https://jsonplaceholder.typicode.com/${slt1}`);
 
   async function searchFor(url: string) {
@@ -55,10 +64,7 @@ function data2(): Promise<Data> {
   }
 }
 
-function data3(): Data {
-  const slt1 = searchItem.value.slt1;
-  const slt2 = searchItem.value.slt2;
-
+function data3(slt1: string, slt2: string): Data {
   if (slt1 !== "users") return [];
   if (!slt2) return [];
   return [{ value: "male" }, { value: "female" }];
