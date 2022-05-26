5e3cefcd3a1bbca6d5a4eb6b8929d84703f4255f
juni <woajun@naver.com>
2022-05-26 14:20:39 +0900

    multiselect 다형성 - Returner

--- src/components/search/conditions/CMultiSelect.vue
+++ src/components/search/conditions/CMultiSelect.vue
@@ -30,17 +30,17 @@ interface SelectSetting {
   dependsOn?: string;
 }
 
-interface NormalSelectSetting extends SelectSetting {
+interface ReceiveSelectSetting extends SelectSetting {
   data: Obj[] | Promise<Obj[]>;
 }
 
-interface NestedSelectSetting extends SelectSetting {
+interface ReferenceSelectSetting extends SelectSetting {
   data: string;
-  dpentdsOn: string;
+  dpendsOn: string;
 }
 
 const props = defineProps<{
-  selects: Array<NormalSelectSetting | NestedSelectSetting>;
+  selects: Array<ReceiveSelectSetting | ReferenceSelectSetting>;
   modelValue: Obj;
 }>();
 
@@ -48,19 +48,30 @@ const emit = defineEmits(["update:modelValue"]);
 
 const selected = computed<Obj>(() => props.modelValue);
 
+class DataReturner {
+  data: Data;
+  constructor(aData: Data) {
+    this.data = aData;
+  }
+}
+
+class ReceiveReturner extends DataReturner {}
+
+class ReferenceReturner extends DataReturner {}
+
 async function dataFor(data: Data, dependsOn?: string): Promise<Obj[]> {
+  const returner = new DataReturner(data);
+  if (typeof data !== "string") return data;
   try {
-    if (typeof data !== "string") return data;
     if (!dependsOn)
       throw new Error(`Data is string. But, don't have dependsOn`);
 
-    const key = data;
     const prntSetting = settingFindBy(dependsOn);
     const prntSelectedObj = await selectedObjFor(
       prntSetting,
       prntSetting.dependsOn
     );
-    return prntSelectedObj[key];
+    return prntSelectedObj[data];
   } catch (error) {
     console.log(error);
     return [];
