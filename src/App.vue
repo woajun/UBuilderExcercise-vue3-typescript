fcdab6f51d629b72420b5e3f05bec5dfc48c8bf0
juni <woajun@naver.com>
2022-05-28 17:18:29 +0900

    object 삭제

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,6 @@
     <br />
     <h3>Function</h3>
     <sample3 />
-    <hr />
-    <br />
-    <h3>Object</h3>
-    <sample4 />
   </div>
 </template>
 
@@ -22,5 +18,4 @@
 import sample1 from "@/pages/SampleMultiSelect_school.vue";
 import sample2 from "@/pages/SampleMultiSelect_array.vue";
 import sample3 from "@/pages/SampleMultiSelect_function.vue";
-import sample4 from "@/pages/SampleMultiSelect_object.vue";
 </script>
